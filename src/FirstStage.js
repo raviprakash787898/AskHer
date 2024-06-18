93a192e15d2db9b40fc0f9f3db29d28ce9ac273d
Ravi Prakash <raviprakash787898@gmail.com>
2024-06-18 21:38:29 +0530

    Update FirstStage.js

--- src/FirstStage.js
+++ src/FirstStage.js
@@ -3,7 +3,7 @@ export default function FirstStage({ parentCallback }) {
         <>
             <img src="https://media.tenor.com/p_VEcmrBsLMAAAAi/manja.gif" alt="Loading..." />
             <h1>Will you hang out with me for a movie? 😅🤗😊</h1>
-            <p>I know a nice place <b>(Elante Mall)</b>, and I think you would like it too. So, let's meet. 
+            <p>I know a nice place, and I think you would like it too. So, let's meet. 
                 Let me know whenever you are available in the upcoming days and we can plan accordingly. 🙂</p>
             <div className="btn">
                 <a href="javascript:void(0)" onClick={() => parentCallback("5")}>Yes</a>
